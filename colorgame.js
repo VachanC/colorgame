e4b559c40baa8950f7eca5b6b4bc118d3612d6e9
vachan <mcb.vachan@gmail.com>
2021-05-14 21:57:42 +0530

    commited into new repository

---
+++ colorgame.js
@@ -0,0 +1,24 @@
+let colors = [
+  "rgb(255, 0, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(255, 255, 0)",
+  "rgb(255, 0, 255)",
+  "rgb(0, 0, 255)",
+];
+
+let squares = document.querySelectorAll(".square");
+let pickedColor = colors[3];
+let colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i].style.background = colors[i];
+  squares[i].addEventListener("click", () => {
+    let clickedColor = squares[i].style.background;
+    if (clickedColor === pickedColor) {
+      alert("clicked");
+    }
+  });
+}
